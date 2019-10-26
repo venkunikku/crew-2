d0f58eeaf2ae4a814effc3850d0bfe8c6e60a277
Venku Buragadda <venku.buragadda.acc@gmail.com>
2019-10-25 21:18:54 -0500

    Added code to recognize cone

--- app/start.py
+++ app/start.py
@@ -1,9 +1,21 @@
 import cv2
 import numpy as np
+from app.utils import find_objects
 
-cap = cv.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+
+cone_obj = find_objects.FindCones()
 
 while True:
-	
-	res, frame = cap.read()
-	
+
+    res, frame = cap.read()
+    flag, frame_back, total_cones, boxes = cone_obj.find_cone(frame)
+
+    cv2.imshow("Cone", frame_back)
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
