39c773a09d033739bd52f4bf9f44120a241e9f81
Venku Buragadda <venku.buragadda.acc@gmail.com>
2019-11-22 21:55:53 -0600

    more clean up and go homne

--- app/start_picamera.py
+++ app/start_picamera.py
@@ -9,7 +9,6 @@ import time
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 
-
 cap = None
 easy = EasyGoPiGo3()
 
@@ -142,7 +141,7 @@ def euclidean_distance(pt1, pt2):
     return math.sqrt(p1 + p2)
 
 
-def gogo():
+def gogo(camera, raw_capture, cone_color="yellow"):
     # global is_cone_centered, forwarded, iscircle_done, temperature, frame_back, cones_data, center_of_the_screen, upper_left, upper_right, lower_left, lower_right, cone_boudning_box, height_range, rect_bottom_mind_point, cone_bottom_mid_points_y, hieght_range_forward, found_required_cone, i, b_box_rec, angle, ref, distance_to_center
     ap = argparse.ArgumentParser()
     ap.add_argument("-r", "--record", required=False, help="Path to the image")
@@ -152,14 +151,9 @@ def gogo():
     record = args["record"]
     isflip = args["flip_imgae"]
 
-    camera = PiCamera()
-    camera.resolution = (640, 480)
-    # camera.vflip = True
-    raw_capture = PiRGBArray(camera, size=(640, 480))
-    time.sleep(1)
-    cone_color = "yellow"
+    # cone_color = "yellow"
     cone_obj = find_objects.FindCones(color=cone_color)
-    aline_cone_to_the_center = False
+
     found_required_cone = False
     is_cone_centered = False
     forwarded = False
@@ -180,35 +174,33 @@ def gogo():
 
         frame_back = center_boundaries(frame_back)
 
-        he, wi, ch = frame_back.shape
-        center_of_the_screen = (wi // 2, he // 2)
-        cv2.circle(frame_back, center_of_the_screen, 5, [255, 0, 0], -1)
-        cv2.line(frame_back, center_of_the_screen, (center_of_the_screen[0] + 30, center_of_the_screen[1]), [0, 255, 0],
-                 3)
+        center_of_screen_coord, horiztl_line_lower_left_coord, horiztl_line_lower_right_coord, horiztl_line_upper_left_coord, horiztl_line_upper_right_coord, \
+        left_bottom_bound_line_coord, left_top_bound_line_coord, right_bottom_bound_line_coord, rigth_top_bound_line_coord, width = screen_coordinates(
+            frame_back)
 
-        # center boundaries
-        left_top, left_bottom = (wi - center_of_the_screen[0] - 20, 0), (wi - center_of_the_screen[0] - 20, he)
-        rigth_top, right_bottom = (wi - center_of_the_screen[0] + 20, 0), (wi - center_of_the_screen[0] + 20, he)
+        cv2.circle(frame_back, center_of_screen_coord, 5, [255, 0, 0], -1)
+        cv2.line(frame_back, center_of_screen_coord, (center_of_screen_coord[0] + 30, center_of_screen_coord[1]),
+                 [0, 255, 0],
+                 3)
 
         # print(F"Left: {left_top},{left_bottom} and Right: {rigth_top}, {right_bottom}")
 
-        cv2.line(frame_back, left_top, left_bottom, [232, 206, 190], 1)
-        cv2.line(frame_back, rigth_top, right_bottom, [232, 206, 190], 1)
+        cv2.line(frame_back, left_top_bound_line_coord, left_bottom_bound_line_coord, [232, 206, 190], 1)
+        cv2.line(frame_back, rigth_top_bound_line_coord, right_bottom_bound_line_coord, [232, 206, 190], 1)
 
-        cv2.line(frame_back, rigth_top, right_bottom, [232, 206, 190], 1)
+        cv2.line(frame_back, rigth_top_bound_line_coord, right_bottom_bound_line_coord, [232, 206, 190], 1)
 
         # Center vertical line
-        cv2.line(frame_back, (center_of_the_screen[0], 0), (center_of_the_screen[0], wi), [0, 255, 0], 1)
+        cv2.line(frame_back, (center_of_screen_coord[0], 0), (center_of_screen_coord[0], width), [0, 255, 0], 1)
 
-        # bottom boundaries
-        upper_left, upper_right = (0, he - 100), (wi, he - 100)
-        lower_left, lower_right = (0, he - 50), (wi, he - 50)
-        cv2.line(frame_back, upper_left, upper_right, [200, 90, 60], 1)
-        cv2.line(frame_back, lower_left, lower_right, [200, 90, 60], 1)
+        cv2.line(frame_back, horiztl_line_upper_left_coord, horiztl_line_upper_right_coord, [200, 90, 60], 1)
+        cv2.line(frame_back, horiztl_line_lower_left_coord, horiztl_line_lower_right_coord, [200, 90, 60], 1)
 
-        cv2.putText(frame_back, f"Upper: {upper_left},{upper_right}", upper_left, cv2.FONT_HERSHEY_SIMPLEX, .5,
+        cv2.putText(frame_back, f"Upper: {horiztl_line_upper_left_coord},{horiztl_line_upper_right_coord}",
+                    horiztl_line_upper_left_coord, cv2.FONT_HERSHEY_SIMPLEX, .5,
                     (0, 255, 255), 1, cv2.LINE_AA)
-        cv2.putText(frame_back, f"Lower: {lower_left},{lower_right}", lower_left, cv2.FONT_HERSHEY_SIMPLEX, .5,
+        cv2.putText(frame_back, f"Lower: {horiztl_line_lower_left_coord},{horiztl_line_lower_right_coord}",
+                    horiztl_line_lower_left_coord, cv2.FONT_HERSHEY_SIMPLEX, .5,
                     (0, 255, 255), 1, cv2.LINE_AA)
 
         cv2.putText(frame_back, f"Temp:{temperature}", (50, 30), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 255, 255), 1,
@@ -221,11 +213,12 @@ def gogo():
                 cone_boudning_box = cones_data['cone-0']['bouding_box_center']
 
                 if not is_cone_centered:
-                    center_boundary_left_right_width = (left_top[0], rigth_top[0])
+                    center_boundary_left_right_width = (left_top_bound_line_coord[0], rigth_top_bound_line_coord[0])
                     print(
                         f"Cone no centered moving for height rnage:{center_boundary_left_right_width} and cone bouding box:{cone_boudning_box}")
 
-                    is_cone_centered = center_cone_to_screen_and_gopio(cone_boudning_box, center_boundary_left_right_width)
+                    is_cone_centered = center_cone_to_screen_and_gopio(cone_boudning_box,
+                                                                       center_boundary_left_right_width)
                 x, y, w, h = boxes[0]
                 x1, y1, x4, y4 = (x, y, x + w, y + h)
                 tl = (x1, y1)
@@ -243,7 +236,8 @@ def gogo():
 
                 if is_cone_centered:
                     cone_lower_rec_boundary_mid_point_height = rect_bottom_mind_point[1]
-                    bottom_boundary_upper_lower_height = (upper_left[1] + 20, lower_left[1] + 20)
+                    bottom_boundary_upper_lower_height = (
+                        horiztl_line_upper_left_coord[1] + 20, horiztl_line_lower_left_coord[1] + 20)
                     if not forwarded:
                         forwarded = get_close_to_the_cone(cone_lower_rec_boundary_mid_point_height,
                                                           bottom_boundary_upper_lower_height)
@@ -260,6 +254,12 @@ def gogo():
                     easy.turn_degrees(180)
                     iscircle_done = True
 
+                if iscircle_done:
+                    go_home(raw_capture, frame_obj, cone_color="red")
+                    raw_capture.truncate(0)
+                    camera.close()
+                    break
+
         cv2.imshow("PI", frame_back)
         key = cv2.waitKey(1) & 0xFF
 
@@ -267,6 +267,77 @@ def gogo():
 
         if key == ord("q"):
             break
+
+
+def go_home(raw_capture, frame_obj, cone_color="orange"):
+    cone_obj = find_objects.FindCones(cone_color=cone_color)
+    raw_capture.truncate(0)
+    found_required_cone = False
+    is_cone_centered = False
+    for frm in frame_obj:
+        frame = frm.array
+
+        flag, frame_back, total_cones, boxes, cones_data = cone_obj.find_cone(frame)
+
+        center_of_screen_coord, horiztl_line_lower_left_coord, horiztl_line_lower_right_coord, horiztl_line_upper_left_coord, horiztl_line_upper_right_coord, \
+        left_bottom_bound_line_coord, left_top_bound_line_coord, right_bottom_bound_line_coord, rigth_top_bound_line_coord, width = screen_coordinates(
+            frame_back)
+        if not found_required_cone:
+            # finding Cone
+            found_required_cone = move_around_to_find_cone(cone_obj, raw_capture, frame_obj)
+            raw_capture.truncate(0)
+            continue
+
+        if found_required_cone:
+            if total_cones > 0:
+                cone_boudning_box = cones_data['cone-0']['bouding_box_center']
+                if not is_cone_centered:
+                    center_boundary_left_right_width = (left_top_bound_line_coord[0], rigth_top_bound_line_coord[0])
+                    print(
+                        f"Cone no centered moving for height rnage:{center_boundary_left_right_width} and cone bouding box:{cone_boudning_box}")
+
+                    is_cone_centered = center_cone_to_screen_and_gopio(cone_boudning_box,
+                                                                       center_boundary_left_right_width)
+
+                x, y, w, h = boxes[0]
+                x1, y1, x4, y4 = (x, y, x + w, y + h)
+                tl = (x1, y1)
+                tr = (x4, y1)
+                br = (x4, y4)
+                bl = (x1, y4)
+                # (tl, tr, br, bl) = boxes[0]
+                (tlblX, tlblY) = midpoint(tl, bl)
+                (trbrX, trbrY) = midpoint(tr, br)
+                rect_bottom_mind_point = midpoint(bl, br)
+
+                if is_cone_centered:
+                    cone_lower_rec_boundary_mid_point_height = rect_bottom_mind_point[1]
+                    bottom_boundary_upper_lower_height = (
+                        horiztl_line_upper_left_coord[1] + 20, horiztl_line_lower_left_coord[1] + 20)
+                    if not forwarded:
+                        forwarded = get_close_to_the_cone(cone_lower_rec_boundary_mid_point_height,
+                                                          bottom_boundary_upper_lower_height)
+                        # Center the cone while you drive forward
+                        center_cone_to_screen_and_gopio(cone_boudning_box, center_boundary_left_right_width)
+                if forwarded:
+                    easy.drive_inches(5)
+                    easy.turn_degrees(360)
+
+        raw_capture.truncate(0)
+
+
+def screen_coordinates(frame_back):
+    height, width, ch = frame_back.shape
+    center_of_screen_coord = (width // 2, height // 2)
+    # center boundaries
+    left_top_bound_line_coord, left_bottom_bound_line_coord = (width - center_of_screen_coord[0] - 20, 0), (
+        width - center_of_screen_coord[0] - 20, height)
+    rigth_top_bound_line_coord, right_bottom_bound_line_coord = (width - center_of_screen_coord[0] + 20, 0), (
+        width - center_of_screen_coord[0] + 20, height)
+    # bottom boundaries
+    horiztl_line_upper_left_coord, horiztl_line_upper_right_coord = (0, height - 100), (width, height - 100)
+    horiztl_line_lower_left_coord, horiztl_line_lower_right_coord = (0, height - 50), (width, height - 50)
+    return center_of_screen_coord, horiztl_line_lower_left_coord, horiztl_line_lower_right_coord, horiztl_line_upper_left_coord, horiztl_line_upper_right_coord, left_bottom_bound_line_coord, left_top_bound_line_coord, right_bottom_bound_line_coord, rigth_top_bound_line_coord, width
 
 
 def get_close_to_the_cone(cone_bottom_mid_points_y, height_range_forward):
@@ -289,4 +360,9 @@ def center_cone_to_screen_and_gopio(cone_boudning_box, height_range):
 
 
 if __name__ == '__main__':
-    gogo()
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    # camera.vflip = True
+    raw_capture = PiRGBArray(camera, size=(640, 480))
+    time.sleep(1)
+    gogo(camera, raw_capture)
