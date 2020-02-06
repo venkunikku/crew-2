065c6f8ea445d7f6be3539e69db089172d9d2b44
Venku Buragadda <venku.buragadda.acc@gmail.com>
2020-02-06 14:21:09 -0600

    [MultiThreading] Adding video stream multi threading class.

---
+++ app/utils/VideoStream.py
@@ -0,0 +1,66 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from threading import Thread
+from multiprocessing import Process
+
+
+class StreamThreaded:
+    def __init__(self, resolution=(320, 240), framerate=32):
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture, format='bgr', use_video_port=True)
+        self.frame = None
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.update, args=()).start()
+
+    def update(self):
+        for f in self.stream:
+            self.frame = f.array
+            self.rawCapture.truncate(0)
+
+            if self.stopped:
+                self.stream.close()
+                self.rawCapture.close()
+                self.camera.close()
+                return
+
+    def read(self):
+        return self.frame
+
+    def stop(self):
+        self.stopped = True
+
+
+class StreamMultiProcssing:
+    def __init__(self, resolution=(320, 240), framerate=32):
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture, format='bgr', use_video_port=True)
+        self.frame = None
+        self.stopped = False
+
+    def start(self):
+        Process(target=self.update, args=()).start()
+
+    def update(self):
+        for f in self.stream:
+            self.frame = f.array
+            self.rawCapture.truncate(0)
+
+            if self.stopped:
+                self.stream.close()
+                self.rawCapture.close()
+                self.camera.close()
+                return
+
+    def read(self):
+        return self.frame
+
+    def stop(self):
+        self.stopped = True
