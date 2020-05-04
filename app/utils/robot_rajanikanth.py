2b542936165cd5fef83bc35de5f494d12e82223f
venku_buragadda <venku.buragadda.acc@gmail.com>
2020-05-04 02:01:15 -0500

    [locomotive] code rearrangement to show video feed boundaries

--- app/utils/robot_rajanikanth.py
+++ app/utils/robot_rajanikanth.py
@@ -280,15 +280,15 @@ class NavigateRajani:
             frame = self.camera.read()
             if not (frame is None):
                 if not self.show_video_limit:
-                    flag, frame_back, total_cones, boxes, cones_data = self.get_cone_coordinates(frame)
-                    if total_cones:
-                        rect_bottom_mind_point = self.get_cone_bottom_mid_point(boxes)
-                        cv2.putText(frame_back, f"bottom: {rect_bottom_mind_point}",
-                                    (int(rect_bottom_mind_point[0]), int(rect_bottom_mind_point[1])),
-                                    cv2.FONT_HERSHEY_SIMPLEX,
-                                    .5, (0, 255, 255), 1, cv2.LINE_AA)
-                        frame = frame_back
-                    frame = NavigateRajani.center_boundaries(frame)
+                    # flag, frame_back, total_cones, boxes, cones_data = self.get_cone_coordinates(frame)
+                    # if total_cones:
+                    #     rect_bottom_mind_point = self.get_cone_bottom_mid_point(boxes)
+                    #     cv2.putText(frame_back, f"bottom: {rect_bottom_mind_point}",
+                    #                 (int(rect_bottom_mind_point[0]), int(rect_bottom_mind_point[1])),
+                    #                 cv2.FONT_HERSHEY_SIMPLEX,
+                    #                 .5, (0, 255, 255), 1, cv2.LINE_AA)
+                    #     frame = frame_back
+                    # frame = NavigateRajani.center_boundaries(frame)
 
                     cv2.line(frame, left_top_bound_line_coord, left_bottom_bound_line_coord, [232, 206, 190], 1)
                     cv2.line(frame, rigth_top_bound_line_coord, right_bottom_bound_line_coord, [232, 206, 190], 1)
