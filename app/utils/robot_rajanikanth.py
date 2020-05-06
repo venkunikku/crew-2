2b13b401991b9b1fba1937bf8e19ee352e913017
venku_buragadda <venku.buragadda.acc@gmail.com>
2020-05-06 14:42:48 -0500

    [locomotive] adjusting

--- app/utils/robot_rajanikanth.py
+++ app/utils/robot_rajanikanth.py
@@ -302,7 +302,8 @@ class NavigateRajani:
                     cv2.line(frame, horiztl_line_upper_left_coord, horiztl_line_upper_right_coord, [200, 90, 60], 1)
                     cv2.line(frame, horiztl_line_lower_left_coord, horiztl_line_lower_right_coord, [200, 90, 60], 1)
 
-                    cv2.line(frame, (horiztl_line_upper_left_coord[0], horiztl_line_upper_left_coord[1]+40), [90, 120, 160], 1)
+                    cv2.line(frame, (horiztl_line_upper_left_coord[0], horiztl_line_upper_left_coord[1]+40),
+                             horiztl_line_upper_right_coord, [90, 120, 160], 1)
 
                     cv2.putText(frame, f"Upper: {horiztl_line_upper_left_coord},{horiztl_line_upper_right_coord}::: {horiztl_line_upper_left_coord[1]+40}",
                                 horiztl_line_upper_left_coord, cv2.FONT_HERSHEY_SIMPLEX, .5,
